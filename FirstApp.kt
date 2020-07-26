3b4047027603f38f28143fedd218de505959f219
Yann Tchoumta <37128739+Tyannherve11@users.noreply.github.com>
2020-07-25 20:49:29 -0500

    Add files via upload
    Kotlin lab week 3

---
+++ FirstApp.kt
@@ -0,0 +1,28 @@
+
+fun main(){
+
+    val age = 5
+    val name = "Rover"
+
+    //print("welcome to kotlin\n")
+    //println(" okc")
+    println("Happy Birthday")
+    print("Yannis\n")
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    println("You're already ${age} years old, ${name}")
+    println("${age} is the very best age to celebrate")
+
+
+}
+
+fun printborder(){
+    repeat(23){
+        print("=")
+    }
+}
